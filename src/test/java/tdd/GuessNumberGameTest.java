09c34095ee8ab99ef1858800e2d21f6e47161345
DolngNothing <1178363215@qq.com>
2020-07-23 20:15:12 +0800

    [christopher] feature: add test should_return_2A2B_when_answer_is_1234_given_1243

--- src/test/java/tdd/GuessNumberGameTest.java
+++ src/test/java/tdd/GuessNumberGameTest.java
@@ -6,14 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GuessNumberGameTest {
     @Test
-    void should_return_4A0B_when_guess_is_1234_given_1234(){
+    void should_return_4A0B_when_answer_is_1234_given_1234(){
         //given
         String guessNumber="1234";
+        String answer="1234";
         //when
-        GuessNumberGame guessNumberGame = new GuessNumberGame(guessNumber);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
         String guessResult=guessNumberGame.guess(guessNumber);
         //should
 
         assertEquals("4A0B",guessResult);
+    }
+
+    @Test
+    void should_return_2A2B_when_answer_is_1234_given_1243(){
+        //given
+        String guessNumber="1243";
+        String answer="1234";
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        String guessResult=guessNumberGame.guess(guessNumber);
+        //should
+        assertEquals("2A2B",guessResult);
     }
 }
